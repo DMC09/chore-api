e42962ac0602df61c0a10ae5b7f216a462b4f9c7
Dharmin <dharmin@seatzapp.com>
2022-10-06 23:22:55 -0500

    added pino logger

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import express from "express";
 import ip from 'ip';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import logger from '../util/logger.js'
 
 dotenv.config();
-const PORT = process.env.SEVER_PORT || 3000
+const PORT = process.env.SEVER_PORT
 const app = express()
 
 app.use(cors({origin: '*'}))
@@ -13,5 +14,5 @@ app.get('/', (req, res) => {
     res.send('hello!')
 })
 app.listen(PORT,()=>{
-    console.info(`Server running on port ${ip.address()}:${PORT}`)
+    logger.info(`Server running on port ${ip.address()}:${PORT}`)
 })
---
+++ util/logger.js
@@ -0,0 +1,13 @@
+import logger from 'pino'
+
+const log = logger({
+        base: { pid:false},
+        transport: { 
+            target:'pino-pretty',
+            options:{ colorized:true}
+
+        },
+        timeStamp: ()=> `,"time":"${new Date().toLocaleString()}"`
+});
+
+export default log

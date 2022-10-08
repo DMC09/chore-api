fc156687685a318f0466da6b49505251e63d331a
Dharmin <dharmin@seatzapp.com>
2022-10-08 14:59:52 -0500

    added generic responses block

---
+++ src/domain/responses.ts
@@ -0,0 +1,9 @@
+// @ts-ignore
+import httpResponse from "express-http-response";
+
+
+     export const OK = httpResponse.OkResponse;
+     export const CREATED = httpResponse.OkResponse;
+     export const BAD_REQUEST = httpResponse.BadRequestResponse;
+     export const NOT_FOUND = httpResponse.NotFoundResponse;
+     export const INTERNAL_SERVER_ERROR = httpResponse.InternalServerErrorResponse
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,29 @@
 import express from "express";
-import ip from 'ip';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import logger from './util/logger.js'
+import ip from "ip";
+import dotenv from "dotenv";
+import cors from "cors";
+import logger from "./util/logger.js";
+// @ts-ignore
+import httpResponse from "express-http-response";
+import {OK,CREATED,BAD_REQUEST,NOT_FOUND,INTERNAL_SERVER_ERROR} from './domain/responses.js'
 
-
+//config
 dotenv.config();
-const PORT = process.env.SEVER_PORT
-const app = express()
+const PORT = process.env.SEVER_PORT;
+const app = express();
+//middle ware
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(httpResponse.Middleware);
 
-app.use(cors({origin: '*'}))
-app.use(express.json())
-app.get('/', (req, res) => {
-    res.send({
-        status:200,
-        message:'this is at test!'
-    })
-})
-app.listen(PORT,()=>{
-    logger.info(`Server  on port ${ip.address()}:${PORT}`)
-})
+app.get("/", (req, res) => {
+//   const OkResponse = httpResponse.OkResponse;
+//   const response = new OkResponse();
+  res.send(new BAD_REQUEST());
+});
+
+
+app.listen(PORT, () => {
+  console.log(httpResponse);
+  logger.info(`Server running on port ${ip.address()}:${PORT}`);
+});

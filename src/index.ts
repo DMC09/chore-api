36a1774cf96407d8f79e9b752cb9cbb4c15e07cb
Dharmin <dharmin@seatzapp.com>
2022-10-17 18:46:35 -0500

    update routes for updating

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import logger from "./util/logger.js";
 // @ts-ignore
 import httpResponse from "express-http-response";
 import {OK,CREATED,BAD_REQUEST,NOT_FOUND,INTERNAL_SERVER_ERROR} from './domain/responses.js'
+import {getItemsFromStore,updateItemsFromStore} from './controller/store.controller.js'
 
 //config
 dotenv.config();
@@ -16,14 +17,24 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(httpResponse.Middleware);
 
+
+//generic test
 app.get("/", (req, res) => {
 //   const OkResponse = httpResponse.OkResponse;
 //   const response = new OkResponse();
-  res.send(new BAD_REQUEST());
+  res.send(new OK());
 });
 
 
+app.get("/general",(req, res) => {
+  getItemsFromStore(req, res);
+})
+app.put("/general/:id",(req, res) => {
+  updateItemsFromStore(req, res);
+})
+
+//initialilzer
 app.listen(PORT, () => {
-  console.log(httpResponse);
+  // console.log(httpResponse);
   logger.info(`Server running on port ${ip.address()}:${PORT}`);
 });

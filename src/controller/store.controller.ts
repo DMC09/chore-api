5f650305598a69537380c0deee0814a74904326d
Dharmin <dharmin@seatzapp.com>
2022-10-17 18:46:20 -0500

    update controller for updating item

--- src/controller/store.controller.ts
+++ src/controller/store.controller.ts
@@ -1,25 +1,62 @@
-import logger from '../util/logger.js'
+import logger from "../util/logger.js";
 import express from "express";
-import database from "../config/mysql.config.js"
-import QUERY from "../query/stores.query.js"
-import {OK,CREATED,BAD_REQUEST,NOT_FOUND,INTERNAL_SERVER_ERROR} from '../domain/responses.js'
+import database from "../config/mysql.config.js";
+import QUERY from "../query/stores.query.js";
+import {
+  OK,
+  CREATED,
+  BAD_REQUEST,
+  NOT_FOUND,
+  INTERNAL_SERVER_ERROR,
+} from "../domain/responses.js";
+import { Request, Response } from "express-serve-static-core";
+import { ParsedQs } from "qs";
 
+export const getItemsFromStore = (req: any, res: any) => {
+  logger.info(`${req.method} ${req.originalUrl} fetching all items`);
+  //Query the Database
+  database.query(
+    QUERY.SELECT_ITEMS_FROM_STORE,
+    "general_store",
+    (error: any, results: any) => {
+      if (!results) {
+        logger.error(error.message);
+        res.send(new OK("No Items Found"));
+      } else {
+        res.send(new OK({ results }, "items retrieved"));
+      }
+    }
+  );
+};
 
+export const updateItemsFromStore = (req: any, res: any) => {
+  console.log(req.body, "body");
+  logger.info(`${req.method} ${req.originalUrl} fetching all items`);
 
-
-
-export const getItemsFromStore = (req: { method: any; originalUrl: any; },res: any) => {
-    console.log(database,'database')
-    logger.info(`${req.method} ${req.originalUrl} fetching all items` );
-    database.query(QUERY.SELECT_ITEMS_FROM_STORE,(error: any,results: any)=>{
-        console.log(error,'error')
-        console.log(results,'results')
-        if(!results){
-            logger.error(error.message);
-            res.send(new OK('No Items Found'))
-        } else{
-            res.send(new OK({results},'items retrieved'))
-        }
-    })
-
-}
+  //check for row matching the id in the DB
+  database.query(
+    QUERY.SELECT_ITEM_FROM_STORE,
+    ["general_store", req.params.id],
+    (error: any, results: any, fields: any) => {
+      if (!results[0]) {
+        logger.info(error);
+        res.send(new NOT_FOUND(`No record found with id:${req.params.id}`));
+      } else {
+        //Perform update to record 
+        logger.info(`${req.method} ${req.originalUrl}, updating store`);
+        database.query(
+          QUERY.UPDATE_ITEMS_FROM_STORE,
+          ["general_store", ...Object.values(req.body), req.params.id],
+          (error: any, results: any, fields: any) => {
+            if (!error) {
+              res.send(new OK({ ...req.body }, "item updated"));
+            } else {
+              logger.error(error);
+              res.send(new INTERNAL_SERVER_ERROR("unable to update item"));
+            }
+          }
+        );
+      }
+    }
+  );
+};

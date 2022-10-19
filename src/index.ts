9b518c0961b2ece5c7f37054af5a6862dfc70a10
Dharmin <dharmin@seatzapp.com>
2022-10-19 16:37:07 -0500

    added routes and methods for creating  and deleting a store.

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,20 @@ import cors from "cors";
 import logger from "./util/logger.js";
 // @ts-ignore
 import httpResponse from "express-http-response";
-import {OK,CREATED,BAD_REQUEST,NOT_FOUND,INTERNAL_SERVER_ERROR} from './domain/responses.js'
-import {getItemsFromStore,updateItemsFromStore,deleteItemsFromStore,addItemsToStore} from './controller/store.controller.js'
+import {
+  OK,
+  CREATED,
+  BAD_REQUEST,
+  NOT_FOUND,
+  INTERNAL_SERVER_ERROR,
+} from "./domain/responses.js";
+import {
+  getItemsFromStore,
+  updateItemsFromStore,
+  deleteItemsFromStore,
+  addItemsToStore,
+} from "./controller/store.controller.js";
+import {createStore, deleteStore} from "./controller/createStore.controller.js"
 
 //config
 dotenv.config();
@@ -17,27 +29,34 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(httpResponse.Middleware);
 
-
 //generic test
 app.get("/", (req, res) => {
-//   const OkResponse = httpResponse.OkResponse;
-//   const response = new OkResponse();
+  //   const OkResponse = httpResponse.OkResponse;
+  //   const response = new OkResponse();
   res.send(new OK());
 });
 
+//store managment service
+app.post("/stores", (req, res) => {
+  createStore(req, res)
+});
+app.delete("/stores", (req, res) => {
+  deleteStore(req, res)
+});
 
-app.get("/:store",(req, res) => {
-  getItemsFromStore(req, res,req.params.store);
-})
-app.put("/:store/:id",(req, res) => {
-  updateItemsFromStore(req, res,req.params.store);
-})
-app.delete("/:store/:id",(req, res) => {
-  deleteItemsFromStore(req, res,req.params.store)
-})
-app.post("/:store/",(req, res) => {
-  addItemsToStore(req, res,req.params.store)
-})
+//individual stores
+app.get("/:store", (req, res) => {
+  getItemsFromStore(req, res, req.params.store);
+});
+app.put("/:store/:id", (req, res) => {
+  updateItemsFromStore(req, res, req.params.store,req.params.id);
+});
+app.delete("/:store/:id", (req, res) => {
+  deleteItemsFromStore(req, res, req.params.store,req.params.id);
+});
+app.post("/:store/", (req, res) => {
+  addItemsToStore(req, res, req.params.store);
+});
 
 //initializer
 app.listen(PORT, () => {

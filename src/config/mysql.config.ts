3017b055555429423dbc37b687bddcb9037ed43d
Dharmin <dharmin@seatzapp.com>
2022-10-18 23:58:55 -0500

    edited for ssl

--- src/config/mysql.config.ts
+++ src/config/mysql.config.ts
@@ -1,15 +1,25 @@
 // @ts-ignore
-import mysql from 'mysql';
-import dotenv from 'dotenv';
+import mysql from "mysql";
+import dotenv from "dotenv";
+import * as fs from "fs";
 
 dotenv.config();
+
+
+
 const pool = mysql.createPool({
-    host:process.env.DB_HOST,
-    port:process.env.DB_PORT,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME,
-    connectionLimit:process.env.DB_CONNECTION_LIMIT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: process.env.DB_CONNECTION_LIMIT,
+  ssl: {
+    sslmode: "verify-full",
+    ca: fs.readFileSync(process.env.DB_ROOT_CERT), // e.g., '/path/to/my/server-ca.pem'
+    key: fs.readFileSync(process.env.DB_KEY), // e.g. '/path/to/my/client-key.pem'
+    cert: fs.readFileSync(process.env.DB_CERT), // e.g. '/path/to/my/client-cert.pem'
+  },
 });
 
 export default pool;

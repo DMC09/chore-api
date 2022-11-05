97b192fcf8d358e36f5a652f301eb4ae16536bf1
Dharmin <dharmin@seatzapp.com>
2022-11-05 14:14:32 -0500

    updated syntax for stores

--- src/query/stores.query.ts
+++ src/query/stores.query.ts
@@ -1,12 +1,13 @@
 const QUERY = {
     MASTER : {
-     CREATE_STORE: "INSERT INTO ?? (table_name,last_updated,created_at,image_url) VALUES (?,UNIX_TIMESTAMP(),?,?)",
+     CREATE_STORE: "INSERT INTO ?? (store_name,last_updated,created_at,image_url) VALUES (?,UNIX_TIMESTAMP(),?,?)",
      GET_STORES: "SELECT * FROM ??",
-     UPDATE_STORE: "UPDATE ?? SET table_item_count = ?, last_updated = UNIX_TIMESTAMP() WHERE table_id = ?",
-     DELETE_STORE: "DELETE FROM ?? where table_id = ?",
+     UPDATE_STORE: "UPDATE ?? SET store_item_count = ?, last_updated = UNIX_TIMESTAMP() WHERE store_id = ?",
+     DELETE_STORE: "DELETE FROM ?? where store_id = ?",
     },
   STORE:{
     GET_ITEMS: "SELECT * FROM ?? ORDER BY added_at ASC LIMIT 100",
+    GET_COUNT: "SELECT COUNT(*) AS activeItemsCount FROM ?? WHERE item_quantity > 0",
     GET_ITEM: "SELECT * from ?? WHERE item_id=?",
     CREATE:"CREATE TABLE ?? (item_id int unsigned NOT NULL AUTO_INCREMENT,item_name varchar(50) DEFAULT NULL,item_quantity int DEFAULT NULL,item_notes varchar(300) DEFAULT NULL,added_a int DEFAULT NULL,image_url varchar(300) DEFAULT NULL,PRIMARY KEY (`item_id`))",
     ADD_ITEMS:

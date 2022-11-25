a6c1c8eb5a97d684c37d93024f66f4fd4c9baa00
Dharmin <dharmin@seatzapp.com>
2022-11-25 14:48:53 -0600

    removed old logic

--- src/controller/store.controller.ts
+++ src/controller/store.controller.ts
@@ -1,6 +1,5 @@
 import logger from "../util/logger.js";
 import express from "express";
-import database from "../config/mysql.config.js";
 import QUERY from "../query/stores.query.js";
 import {
   OK,
@@ -10,19 +9,17 @@ import {
   INTERNAL_SERVER_ERROR,
 } from "../domain/responses.js";
 import { Request, Response } from "express-serve-static-core";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+});
 
-export const getAllStores = (req: express.Request, res: express.Response) =>{
+export const getAllStores = async(req: express.Request, res: express.Response) => {
   logger.info(`${req.method} ${req.originalUrl} getting stores`);
-  database.query(QUERY.MASTER.GET_STORES,["master"],(error: any,results: any) =>{
-    if(!error){
-      logger.info(error);
-      res.send(new OK( results , "stores retrieved"));
-    } else{
-      res.send(new OK("No stores found"));
-    }
-  })
-}
+  const allStoresData = await prisma.stores.findMany()
+    res.send(new OK(allStoresData))
+};
 
 export const createStore = (req: express.Request, res: express.Response) => {
   //extract data
@@ -30,62 +27,15 @@ export const createStore = (req: express.Request, res: express.Response) => {
 
   logger.info(`${req.method} ${req.originalUrl} creating new store`);
 
-  //create the store and if successful create an entry for in the master
-  database.query(
-    QUERY.STORE.CREATE,
-    storeName,
-    (error: Error, results: any) => {
-      if (!error) {
-        database.query(
-          QUERY.MASTER.CREATE_STORE,
-          ["master", storeName, imageUrl],
-          (error: Error, results: any) => {
-            if (!error) {
-              res.send(new OK("Store has been created"));
-            } else {
-              logger.info(error);
-              res.send(
-                new INTERNAL_SERVER_ERROR("unable to add store to database")
-              );
-            }
-          }
-        );
-      } else {
-        logger.info(error);
-      }
-    }
-  );
+
 };
 
 export const deleteStore = (req: express.Request, res: express.Response) => {
-//extract the items
-  const {storeName,storeId} = req.body
+  //extract the items
+  const { storeName, storeId } = req.body;
 
   logger.info(`${req.method} ${req.originalUrl} deleting store`);
   //Drop the store and then delete it from the master
-  database.query(
-    QUERY.STORE.DELETE,
-    storeName,
-    (error: Error, results: any) => {
-      if (!error) {
-          database.query(QUERY.MASTER.DELETE_STORE,["master",storeId],(error: Error, results: any) => {
-              if(!error) {
-                  res.send(new OK("store has been removed from database"));
-            } else {
-                logger.info(error);
-                res.send(
-                  new INTERNAL_SERVER_ERROR("unable to remove store from database")
-                );
-            }
-        })
-      } else {
-        logger.info(error);
-        res.send(
-          new INTERNAL_SERVER_ERROR("unable to remove store from database")
-        );
-      }
-    }
-  );
 
   // res.send(...Object.values(req.body))
 };
--- src/controller/storeItem.controller.ts
+++ src/controller/storeItem.controller.ts
@@ -1,6 +1,6 @@
 import logger from "../util/logger.js";
 import express from "express";
-import database from "../config/mysql.config.js";
+
 import QUERY from "../query/stores.query.js";
 import {
   OK,
@@ -18,18 +18,6 @@ export const getItemsFromStore = (
 ) => {
   logger.info(`${req.method} ${req.originalUrl} fetching all items`);
   //Query the Database
-  database.query(
-    QUERY.STORE.GET_ITEMS,
-    storeName,
-    (error: any, results: any) => {
-      if (!results) {
-        logger.error(error.message);
-        res.send(new OK("No Items Found"));
-      } else {
-        res.send(new OK(results , "items retrieved"));
-      }
-    }
-  );
 };
 
 export const updateItemsFromStore = (
@@ -40,60 +28,6 @@ export const updateItemsFromStore = (
 ) => {
   logger.info(`${req.method} ${req.originalUrl} fetching all items`);
   //check for row matching the id in the DB
-  database.query(
-    QUERY.STORE.GET_ITEM,
-    [storeName, itemId],
-    (error: any, results: any, fields: any) => {
-      if (!results[0]) {
-        logger.info(error);
-        res.send(new NOT_FOUND(`No record found with id:${itemId}`));
-      } else {
-        //Perform update to record
-        logger.info(`${req.method} ${req.originalUrl}, updating store`);
-        database.query(
-          QUERY.STORE.UPDATE_ITEMS,
-          [
-            storeName,
-            req.body.itemName,
-            req.body.itemQuantity,
-            req.body.itemNotes,
-            req.body.url,
-            itemId,
-          ],
-          (error: any, results: any, fields: any) => {
-            if (!error) {
-              console.log(results);
-              //get the active item count
-              database.query(
-                QUERY.STORE.GET_COUNT,
-                storeName,
-                (error: any, results: any) => {
-                  if (!error) {
-                    const count = results[0].activeItemsCount;
-                    database.query(
-                      //update the master
-                      QUERY.MASTER.UPDATE_STORE,
-                      ["master", count, req.body.storeId],
-                      (error: any, results: any) => {
-                        if (!error) {
-                          res.send(new OK("item updated"));
-                        } else {
-                          logger.info(error);
-                        }
-                      }
-                    );
-                  }
-                }
-              );
-            } else {
-              logger.error(error);
-              res.send(new INTERNAL_SERVER_ERROR("unable to update item"));
-            }
-          }
-        );
-      }
-    }
-  );
 };
 
 export const deleteItemsFromStore = (
@@ -104,39 +38,7 @@ export const deleteItemsFromStore = (
 ) => {
   const storeId = req.body.storeId;
   logger.info(`${req.method} ${req.originalUrl}, Deleting Item from Store`);
-  database.query(
-    QUERY.STORE.DELETE_ITEMS,
-    [storeName, itemId],
-    (error: any, results: any) => {
-      if (results.affectedRows > 0) {
-        database.query(
-          QUERY.STORE.GET_COUNT,
-          storeName,
-          (error: any, results: any) => {
-            if (!error) {
-              const count = results[0].activeItemsCount;
-              database.query(
-                QUERY.MASTER.UPDATE_STORE,
-                ["master", count, storeId],
-                (err: any, result: any) => {
-                  if (!error) {
-                    res.send(new OK("item deleted"));
-                  } else {
-                    logger.info(error);
-                  }
-                }
-              );
-            } else {
-              logger.info(error, "after getting count");
-            }
-          }
-        );
-      } else {
-        logger.info(error, "after deleting items");
-        res.send(new INTERNAL_SERVER_ERROR("unable to delete item"));
-      }
-    }
-  );
+
 };
 
 export const addItemsToStore = (
@@ -148,39 +50,4 @@ export const addItemsToStore = (
   const { itemName, itemQuantity, itemNotes, imageUrl, storeId } = req.body;
   logger.info(`${req.method} ${req.originalUrl} Adding item(s) to store`);
   //add Items to store
-  database.query(
-    QUERY.STORE.ADD_ITEMS,
-    [storeName, itemName, itemQuantity, itemNotes, imageUrl],
-    (error: Error, results: any) => {
-      if (!results) {
-        logger.error(error.message, "error after adding ");
-        res.send(new INTERNAL_SERVER_ERROR("unable to add item"));
-      } else {
-        //if successful get the count of the active items and update the master
-        // console.log(results.insertId,'id');
-        database.query(
-          QUERY.STORE.GET_COUNT,
-          storeName,
-          (error: any, results: any) => {
-            if (!error) {
-              const count = results[0].activeItemsCount;
-              database.query(
-                QUERY.MASTER.UPDATE_STORE,
-                ["master", count, storeId],
-                (error: any, results: any) => {
-                  if (!error) {
-                    res.send(new OK("added item and updated count"));
-                  } else {
-                    logger.info(error, "after updated master");
-                  }
-                }
-              );
-            } else {
-              logger.info(error, "after getting the count");
-            }
-          }
-        );
-      }
-    }
-  );
 };
